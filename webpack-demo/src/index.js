942ac70817f97e77256a2ab5739b11e4832aa8c3
Rayhan <nofussdufus@gmail.com>
2021-11-30 13:54:51 +0000

    Added output handling of compiling js files using htmlwebpack  plugin

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,23 +1,17 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.jpg';
-import Data from './data.xml';
-import Notes from './data.csv';
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
 
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-  element.appendChild(myIcon);
-
-  console.log(Data);
-  console.log(Notes);
+  element.appendChild(btn);
 
   return element;
 }
