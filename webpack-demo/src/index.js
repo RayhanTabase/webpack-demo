f50c15ce27d9e1b6be7c5c2946554655f77e78f6
Rayhan <nofussdufus@gmail.com>
2021-11-30 13:37:38 +0000

    Add loaders for style/css, images, csv, xml

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,9 +1,23 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.jpg';
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  console.log(Data);
+  console.log(Notes);
 
   return element;
 }
